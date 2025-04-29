2b80490c10e08e48b2bffb9b31a90db53097fff1
Raushan Kumar <kumarraushanr50@gmail.com>
2025-04-30 00:15:12 +0530

    Add one more problem of Day 26

--- assignment/day26/BubbleSortInc.java
+++ assignment/day26/BubbleSortInc.java
@@ -2,26 +2,27 @@ package assignment.day26;
 
 import java.util.Scanner;
 
-public class BubbleSortDesc {
+public class BubbleSortInc {
+
   public static void display(int[] nums){
     for(int i=0;i<nums.length;i++){
       System.out.print(nums[i]+" ");
     }
     System.out.println();
   }
-
-  public static void bubbleSortDesc(int[] nums, int n){
+  
+  public static void bubbleSortInc(int[] arr, int n){
     for(int i=0;i<n-1;i++){
       for(int j=0;j<n-i-1;j++){
-        if(nums[j] < nums[j+1]){
-          int temp = nums[j];
-          nums[j] = nums[j+1];
-          nums[j+1] = temp;
+        if(arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
         }
       }
     }
 
-    display(nums);
+    display(arr);
   }
 
   public static void main(String[] args) {
@@ -35,8 +36,8 @@ public class BubbleSortDesc {
       arr[i] = sc.nextInt();
     }
 
-    bubbleSortDesc(arr, n);
-    
+    bubbleSortInc(arr,n);
+
     sc.close();
   }
 }
