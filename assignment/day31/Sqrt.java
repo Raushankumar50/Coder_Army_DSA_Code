dbccb48dc9319596e64c44af5ffbc4283e7c14b3
Raushan Kumar <kumarraushanr50@gmail.com>
2025-05-12 10:52:12 +0530

    Add one mor problem of Day 31

---
+++ assignment/day31/Sqrt.java
@@ -0,0 +1,36 @@
+package assignment.day31;
+
+import java.util.Scanner;
+
+public class Sqrt {
+
+  public static int sqrt(int x){
+    if(x==0 || x==1){
+      return x;
+    }
+
+    int st = 1, end = x;
+    while (st <= end) {
+      int mid = st + (end-st)/2;
+      if((long) mid*mid > (long)x){
+        end = mid-1;
+      } else if(mid*mid == x){
+        return mid;
+      } else {
+        st = mid+1;
+      }
+    }
+
+    return Math.round(end);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a value : ");
+    int x = sc.nextInt();
+
+    System.out.println(sqrt(x));
+
+    sc.close();
+  }
+}
